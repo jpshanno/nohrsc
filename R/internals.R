502f9778a2c2e32bcea7681c191942dba6828d9f
Joe Shannon <jpshanno@mtu.edu>
2019-10-10 15:58:42 -0400

    Minimum viable product of downloading NOHRSC data

--- R/internals.R
+++ R/internals.R
@@ -1,3 +1,15 @@
+nsa_path <-
+  function(path = ""){
+    if(path != ""){
+      # Add checks for existence & write access
+      return(path)
+    }
+
+    # rappdirs
+
+    stop("path must be specified, future versions will allows automatic storage.")
+  }
+
 check_connection <-
   function(){
     if(!curl::has_internet()){
@@ -6,64 +18,332 @@ check_connection <-
   }
 
 look_up_product <-
-  function(prod){
+  function(product){
 
-    prod <-
-      toupper(prod)
+    product <-
+      tolower(product)
 
     good_prods <-
-      c("SWE", "MELT")
+      c("liquid precipitation",
+        "snow precipitation",
+        "snow water equivalent",
+        "snow depth",
+        "snow pack average temperature",
+        "blowing snow sublimation",
+        "snow melt",
+        "snow pack sublimation")
 
-    nohrsc_code <-
-      switch (prod,
-              'SWE' = action,
-              'MELT' = )
+    if(!all(product %in% good_prods)){
+      stop("product must be specified as a character vector of one or more of '", paste(good_prods, collapse = "', '"), "'")
+    }
+
+    vapply(product,
+           switch,
+           character(1),
+           "liquid precipitation" = "1025",
+           "snow precipitation" = "1025",
+           "snow water equivalent" = "1034",
+           "snow depth" = "1036",
+           "snow pack average temperature" = "1038",
+           "blowing snow sublimation" = "1039",
+           "snow melt" = "1044",
+           "snow pack sublimation" = "1050",
+           USE.NAMES = FALSE)
+  }
+
+build_product <-
+  function(prod, date){
+    # Build product name based on naming conventions
+  }
+
+format_date <-
+  function(start.date, end.date = NULL){
+
+    stopifnot(is.character(start.date) | lubridate::is.timepoint(start.date))
+
+    if(is.character(start.date)){
+      if(!all(grepl("\\d{4}-\\d{1,2}-\\d{1,2}", start.date))){
+        stop("date must be a character written as 'YYYY-MM-DD' or a datetime object")
+      }
+    }
+
+    if(is.character(start.date)){
+      start.date <-
+        lubridate::ymd(start.date)
+    }
+
+    if(is.null(end.date)){
+      date <- start.date
+    } else {
+
+      stopifnot(is.character(end.date) | lubridate::is.timepoint(end.date))
+
+      if(is.character(end.date)){
+        if(!all(grepl("\\d{4}-\\d{1,2}-\\d{1,2}", end.date))){
+          stop("date must be a character written as 'YYYY-MM-DD' or a datetime object")
+        }
+      }
+
+      if(is.character(end.date)){
+        end.date <-
+          lubridate::ymd(end.date)
+      }
+
+      date <-
+        seq(start.date, end.date, 1)
+    }
+
+    lapply(date,
+           function(date){
+             year <-
+               format(date, "%Y")
+
+             month <-
+               format(date, "%m")
+
+             month_abb <-
+               format(date, "%b")
+
+             mday <-
+               format(date, "%d")
+
+             c(year = year,
+               month = month,
+               month_abb = month_abb,
+               mday = mday,
+               longdate = paste0(year, month, mday))
+           }
+    )
   }
 
 build_url <-
-  function(region, prod, date){
+  function(region, nsa_date){
 
-    stopifnot(region %in% c("CONUS", "North America"))
-
-    if(region == "CONUS"){
-      folder <- "masked"
-    } else {
-      folder <- "unmasked"
-    }
-
-    prod <-
-      look_up_product(prod)
+    # Add tests:
+    # identical("ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/unmasked/2010/02_Feb/SNODAS_unmasked_20100208.tar", build_url(date = "2010-02-08"))
+    # ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/unmasked/2010/02_Feb/SNODAS_unmasked_20100208.tar
+    # identical("ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/masked/2010/04_Apr/SNODAS_20100404.tar", build_url(region = "CONUS", date = "2010-04-04"))
+    # ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/masked/2010/04_Apr/SNODAS_20100404.tar
 
 
-    # Build urls for all combinations of data at once
-    dates <- 1:10
-    products <- letters[1:3]
-    paste(expand.grid(dates, products)[[1]], expand.grid(dates, products)[[2]], sep = ".")
+    base_url <-
+      "ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/"
+
+    folder <-
+      ifelse(region == "CONUS",
+             "masked",
+             "unmasked")
+
+    url_params <-
+      vapply(nsa_date,
+             function(date){
+               paste0(folder, "/",
+                      date["year"], "/",
+                      date["month"], "_", date["month_abb"], "/",
+                      "SNODAS_", ifelse(folder == "masked", "", paste0(folder, "_")), date["longdate"], ".tar")
+             },
+             character(1))
+
+
+    paste0(base_url, url_params)
   }
 
 build_basefile <-
-  function(prod){
+  function(product){
     # Use product info & date to build archive & raster file basename
+    # Build urls for all combinations of data at once
+    # dates <- 1:10
+    # products <- letters[1:3]
+    # paste(expand.grid(dates, products)[[1]], expand.grid(dates, products)[[2]], sep = ".")
+
+    product <-
+      look_up_product(product)
+
   }
 
 create_hdr <-
-  function(base_file){
-    # basefile should be the raster file name with no extension
-    file <-
-      paste0(base_file, ".hdr")
+  function(file){
+    # THIS NEEDS TO BE UPDATED TO GET ulxmap, ulymap, xdim, ydim FROM SNODAS HEADER
+    # OR AT LEAST CHECKED THAT IT IS ALWAYS THE SAME VALUE
+
+    hdr_file <-
+      sub("bil$", "hdr", file)
+
+    txt_file <-
+      sub("bil$", "txt", file)
+
+    NBITS <-
+      8*as.numeric(sub("\\D*", "", grep("(?<=Data bytes per pixel: )[0-9]*", readLines(txt_file), value = TRUE, perl = TRUE)))
+    NCOLS <-
+      sub("\\D*", "", grep("(?<=Number of columns)[0-9]*", readLines(txt_file), value = TRUE, perl = TRUE))
+    NROWS <-
+      sub("\\D*", "", grep("(?<=Number of rows)[0-9]*", readLines(txt_file), value = TRUE, perl = TRUE))
+    ULXMP <-
+      sub("\\D*", "", grep("(?<=Benchmark x-axis coordinate)[0-9]*", readLines(txt_file), value = TRUE, perl = TRUE))
+    ULYMP <-
+      sub("\\D*", "", grep("(?<=Benchmark y-axis coordinate)[0-9]*", readLines(txt_file), value = TRUE, perl = TRUE))
+    XDIM <-
+      sub("\\D*", "", grep("(?<=X-axis resolution)[0-9]*", readLines(txt_file), value = TRUE, perl = TRUE))
+    YDIM <-
+      sub("\\D*", "", grep("(?<=Y-axis resolution)[0-9]*", readLines(txt_file), value = TRUE, perl = TRUE))
 
     hdr_content <-
       c("byteorder M",
         "layout bil",
         "nbands 1",
-        "nbits 16",
-        "ncols 6935",
-        "nrows 3351",
-        "ulxmap -124.729583333333",
-        "ulymap 52.8704166666666",
-        "xdim 0.00833333333333333",
-        "ydim 0.00833333333333333")
+        paste("nbits", NBITS),
+        paste("ncols", NCOLS),
+        paste("nrows", NROWS),
+        paste("ulxmap", ULXMP),
+        paste("ulymap", ULYMP),
+        paste("xdim", XDIM),
+        paste("ydim", YDIM))
 
-    write_file(hdr_content,
-               file)
+    writeLines(hdr_content,
+               hdr_file)
+
+    unlink(txt_file)
+  }
+
+create_prj <-
+  function(file){
+    prj_file <-
+      sub("bil$", "prj", file)
+
+    prj_content <-
+      'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]'
+
+    prj_out <-
+      writeLines(text = prj_content,
+                 prj_file)
+  }
+
+# download_archive <-
+#   function(){
+#
+#   }
+
+extract_archive <-
+  function(archive, product){
+    combos <-
+      expand.grid(archive = archive,
+                  product = product,
+                  stringsAsFactors = FALSE)
+
+    extracted_files <-
+      mapply(
+        function(archive, product){
+
+          longdate <-
+            sub(".*(\\d{8}).tar", "\\1", archive, perl = TRUE)
+
+          region <-
+            ifelse(grepl("SNODAS_unmasked_", archive),
+                   "North America",
+                   "CONUS")
+
+          files <-
+            build_filenames(product, longdate, region)
+
+          tar_results <-
+            untar(archive,
+                  files = files[1:2],
+                  exdir = dirname(archive))
+
+          unzipped_files <-
+            vapply(file.path(dirname(archive), files[1:2]),
+                   R.utils::gunzip,
+                   character(1),
+                   overwrite = TRUE,
+                   USE.NAMES = FALSE)
+
+          bil_file <-
+            sub(".dat$", ".bil", unzipped_files[1])
+
+          renames <-
+            file.rename(unzipped_files[1],
+                        bil_file)
+
+          bil_file
+        },
+        combos[["archive"]],
+        combos[["product"]],
+        USE.NAMES = FALSE)
+
+    c(extracted_files)
+  }
+
+build_filenames <-
+  function(product, longdate, region){
+    # rr_mmmffppppSvvvvTttttaaaaTSyyyymmddhhIP00Z.xxx.gz
+
+    # change parameter name to nsa_date
+
+    # combos <- expand.grid(product = product,
+    #                       date = nsa_date,
+    #                       stringsAsFactors = FALSE)
+
+    # lapply(product,
+    #        function(product){
+             rr <- ifelse(region == "CONUS", "us", "zz")
+
+             mmm <- "ssm"
+
+             ff <- ifelse(grepl("precipitation$", product),
+                          "v0",
+                          "v1")
+
+             pppp <- look_up_product(product)
+
+             S <- ifelse(grepl("precipitation$", product),
+                         "S",
+                         "")
+
+             vvvv <- switch(product,
+                            "liquid precipitation" = "lL00",
+                            "snow precipitation" = "lL01",
+                            "snow water equivalent" = "tS__",
+                            "snow depth" = "tS__",
+                            "snow pack average temperature" = "wS__",
+                            "blowing snow sublimation" = "lL00",
+                            "snow melt" = "bS__",
+                            "snow pack sublimation" = "lL00")
+
+             Ttttt <- switch(product,
+                             "liquid precipitation" = "T0024",
+                             "snow precipitation" = "T0024",
+                             "snow water equivalent" = "T0001",
+                             "snow depth" = "T0001",
+                             "snow pack average temperature" = "A0024",
+                             "blowing snow sublimation" = "T0024",
+                             "snow melt" = "T0001",
+                             "snow pack sublimation" = "T0024")
+
+             aaaa <- "TTNA"
+
+             TSyyyymmddhh <- paste0("TS", longdate, "05")
+
+             I <- ifelse(Ttttt == "T0001", "H", "D")
+
+             P00Z <- ifelse(product %in% c("liquid precipitation",
+                                           "snow precipitation",
+                                           "snow water equivalent",
+                                           "snow depth",
+                                           "snow pack average temperature"),
+                            "P001",
+                            "P000")
+
+             basename <-
+               paste0(rr, "_", mmm, ff, pppp, S, vvvv, Ttttt, aaaa, TSyyyymmddhh, I, P00Z)
+
+               c(datgz = paste0(basename, ".dat.gz"),
+                 txtgz = paste0(basename, ".txt.gz")
+                 # ,dat = paste0(basename, ".dat"),
+                 # txt = paste0(basename, ".txt")
+                 # ,bil = paste0(basename, ".bil"),
+                 # hdr = paste0(basename, ".hdr"),
+                 # prj = paste0(basename, ".prj")
+                 )
+           # }
+# )
   }
---
+++ R/nsa_get_snodas.R
@@ -0,0 +1,56 @@
+#' Download and extract national snow anlaysis data
+#'
+#' @param product A character vector of one or more prodcuts available via
+#'   NOHRSC Acceptable values are "liquid precipitation", "snow precipitation",
+#'   "snow water equivalent", "snow depth", "snow pack average temperature",
+#'   "blowing snow sublimation", "snow melt", and "snow pack sublimation"
+#' @param start.date A start date supplied as a character string ("YYYY-MM-DD")
+#'   or a datetime object
+#' @param end.date NULL if only one date is wanted, otherwise the end date of a
+#'   time span formated as start.date
+#' @param region Either "CONUS" or "North America" indicating if the results
+#'   should be contained to the continental US or not
+#' @param path The output path to save the raster data
+#' @param delete.archive A logical indicating if the downloaded archives be
+#'   deleted after the rasters have been extracted. Deafults to FALSE
+#' @param force.download Not currently used
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # nsa_get_snodas(c("snow water equivalent"), start.date = "2010-01-06", end.date = "2010-01-08", path = "..")
+nsa_get_snodas <-
+  function(product, start.date, end.date = NULL, region = "North America", path = "", delete.archive = FALSE, force.download = FALSE){
+
+    stopifnot(region %in% c("CONUS", "North America"))
+
+    stopifnot(is.character(region))
+
+    date <-
+      format_date(start.date, end.date)
+
+    # Would check for existing data here and then download and extract
+
+    url <-
+      build_url(region, date)
+
+    archive_file <-
+      file.path(nsa_path(path), regmatches(url, regexpr("SNODAS_.*.tar$", url)))
+
+    if(sum(!file.exists(archive_file)) != 0){
+      download.file(url,
+                    archive_file)
+    }
+
+    product_files <-
+      extract_archive(archive_file, product)
+
+    hdr <- lapply(product_files, create_hdr)
+    prj <- lapply(product_files, create_prj)
+
+    if(delete.archive){
+      removed <- lapply(archive_file,
+                        unlink)
+    }
+  }
